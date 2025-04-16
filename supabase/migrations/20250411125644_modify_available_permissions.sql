5221be62c63dfd66de0c374635a4db9762cb1327
malrig <g-a-b@hotmail.co.uk>
2025-04-16 17:12:24 +0100

    Fix up permissions in Supabase

--- supabase/migrations/20250411125644_modify_available_permissions.sql
+++ supabase/migrations/20250411125644_modify_available_permissions.sql
@@ -1,7 +1,2 @@
-alter type "public"."app_permission" rename to "app_permission__old_version_to_be_dropped";
-
-create type "public"."app_permission" as enum ('data-store.update', 'data-store.delete');
-
-alter table "public"."role_permissions" alter column permission type "public"."app_permission" using permission::text::"public"."app_permission";
-
-drop type "public"."app_permission__old_version_to_be_dropped";
+ALTER TYPE "public"."app_permission" RENAME VALUE 'datastore.update' TO 'data-store.update';
+ALTER TYPE "public"."app_permission" RENAME VALUE 'datastore.delete' TO 'data-store.delete';

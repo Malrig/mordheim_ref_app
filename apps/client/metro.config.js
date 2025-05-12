ab9597482f7b641b107fe923dd3f4f433602e328
malrig <g-a-b@hotmail.co.uk>
2025-05-12 21:06:42 +0100

    Temp fix for Supabase + Expo

--- apps/client/metro.config.js
+++ apps/client/metro.config.js
@@ -10,6 +10,9 @@ const monorepoRoot = path.resolve(projectRoot, '../..');
 
 const config = getDefaultConfig(projectRoot);
 
+// Temporary fix for Node.js module resolution issue
+config.resolver.unstable_enablePackageExports = false
+
 // 1. Watch all files within the monorepo
 config.watchFolders = [monorepoRoot];
 // 2. Let Metro know where to resolve packages and in what order
